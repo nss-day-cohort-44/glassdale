76ecf3f522bb94fbece52121f7f3f48800ae2b1f
Jisie DM <jisie.david@gmail.com>
2020-10-16 16:30:20 -0500

    Adding the convictions drop menu: get list of convictions from API, build select tag with options and append to DOM

---
+++ scripts/convictions/ConvictionProvider.js
@@ -0,0 +1,19 @@
+let convictions = []
+
+export const useConvictions = () => convictions.slice()
+
+export const getConvictions = () => {
+  /*
+      Load database state into application state with a fetch().
+      Make sure the last `then()` sets the local `convictions`
+      variable to what is in the response from the API.
+  */
+  return fetch("https://criminals.glassdale.us/crimes")
+    .then(response => {
+      console.log("response before it's json", response)
+      return response.json()
+    })
+    .then(convictionsArray => {
+      convictions = convictionsArray
+    })
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
-import { CriminalList } from "./criminals/CriminalList.js";
+import { ConvictionSelect } from "./convictions/ConvictionSelect.js";
 import { OfficerList } from "./officers/OfficerList.js";
 
-CriminalList()
 OfficerList()
+ConvictionSelect()
