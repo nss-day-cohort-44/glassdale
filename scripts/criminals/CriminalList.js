7035155cb8fc8ebe34105d8cf50a183563ce287e
Jisie DM <jisie.david@gmail.com>
2020-10-20 13:14:12 -0500

    Displaying the criminals

---
+++ scripts/criminals/CriminalList.js
@@ -0,0 +1,27 @@
+import { getCriminals, useCriminals } from "./CriminalProvider.js"
+import { Criminal } from "./Criminal.js"
+
+const criminalsContainer = document.querySelector(".criminalsContainer")
+
+export const CriminalList = () => {
+
+  getCriminals()
+    .then(() => {
+      const criminalArray = useCriminals()
+
+      let criminalsHTMLRepresentations = ""
+      for (const criminal of criminalArray) {
+
+        criminalsHTMLRepresentations += Criminal(criminal)
+
+        criminalsContainer.innerHTML = `
+          <h3>Glassdale Criminals</h3>
+          <section class="criminalsList">
+            ${criminalsHTMLRepresentations}
+          </section>
+        `
+
+      }
+    })
+
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,8 @@
 import { ConvictionSelect } from "./convictions/ConvictionSelect.js";
+import { Criminal } from "./criminals/Criminal.js";
+import { CriminalList } from "./criminals/CriminalList.js";
 import { OfficerList } from "./officers/OfficerList.js";
 
 OfficerList()
 ConvictionSelect()
+CriminalList()
