5d51a35ad9fa43932956d5c1f607570353366d2b
scottsilver <scottsilver47@gmail.com>
2020-11-09 10:47:11 -0600

    added delete functionality for notes

--- scripts/notes/NotesDataProvider.js
+++ scripts/notes/NotesDataProvider.js
@@ -36,3 +36,11 @@ export const saveNote = (note) => {
     .then(getNotes)
     .then(dispatchStateChangeEvent)
 }
+
+export const deleteNote = noteId => {
+    return fetch(`http://localhost:8088/notes/${noteId}`, {
+        method: "DELETE"
+    })
+        .then(getNotes)
+}
+
